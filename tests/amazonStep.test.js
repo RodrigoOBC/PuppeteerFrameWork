4ab16d6394cc75f31be3ad73714f5c58809303be
rodrigo.cabral <rodrigo.oliveiracabral@hotmail.com>
2021-11-23 10:55:17 -0300

    Colando Page

--- tests/amazonStep.test.js
+++ tests/amazonStep.test.js
@@ -1,65 +1,45 @@
 const expect = require('chai').expect
 const puppeteer = require('puppeteer')
+let AmaozonElementos = require("../Page/AmazonPage");
 
 
 describe('Gerenciar Carrinho de compra da Amazon', () => {
     let page = ""
     let browser = ""
     it('Inicio o Browser', async function () {
-        browser = await puppeteer.launch({ headless: false })
+        AmaozonElementos = new AmaozonElementos()
+        browser = await puppeteer.launch({ headless: false ,defaultViewport: null, args: ['--start-maximized']  })
         page = await browser.newPage()
+        await page.setViewport({ width: 1366, height: 768});
     })
 
     it('Adciono protudo ao carrinho ', async function () {
-        await page.goto('https://www.amazon.com.br/')
-        await page.waitForSelector('#twotabsearchtextbox')
-        await page.click('input[id="twotabsearchtextbox"]')
-        await page.type('input[id="twotabsearchtextbox"]', 'Batman o longo dia das Bruxas')
-        await page.click('div[class="nav-search-submit nav-sprite"]')
-        await page.waitForXPath('//span[text()="Batman - O Longo Dia das Bruxas - Edição Definitiva"]')
-        let produto = await page.$x('//span[text()="Batman - O Longo Dia das Bruxas - Edição Definitiva"]');
+        await page.goto(AmaozonElementos.UrlPage)
+        await page.waitForSelector(AmaozonElementos.CampoPesquisar)
+        await page.click(AmaozonElementos.CampoPesquisar)
+        await page.type(AmaozonElementos.CampoPesquisar, 'Batman o longo dia das Bruxas')
+        await page.click(AmaozonElementos.BotaoPesquisar)
+        await page.waitForXPath(AmaozonElementos.Produto)
+        let produto = await page.$x(AmaozonElementos.Produto);
         await produto[0].click()
-        await page.waitForXPath('//*[@id="productTitle"]')
-        let element = await page.$x('//*[@id="productTitle"]')
+        await page.waitForXPath(AmaozonElementos.ProdutoTitulo)
+        let element = await page.$x(AmaozonElementos.ProdutoTitulo)
         let value = await page.evaluate(el => el.textContent, element[0])
         expect('\nBatman - O Longo Dia das Bruxas - Edição Definitiva\n').to.equal(value)
-        await page.click("#add-to-cart-button")
-        await page.waitForXPath('//*[@id="nav-cart-count-container"]/span[2]')
-        let carrinho = await page.$x('//*[@id="nav-cart-count-container"]/span[2]');
+        await page.click(AmaozonElementos.AdicionarCarrinho)
+        await page.waitForXPath(AmaozonElementos.CarinhoBotão)
+        let carrinho = await page.$x(AmaozonElementos.CarinhoBotão);
         await carrinho[0].click()
-        await page.waitForXPath('//span[@class="a-truncate-full a-offscreen"]')
-        element = await page.$x('//span[@class="a-truncate-full a-offscreen"]')
+        await page.waitForXPath(AmaozonElementos.ProdutoNoCarrinho)
+        element = await page.$x(AmaozonElementos.ProdutoNoCarrinho)
         value = await page.evaluate(el => el.textContent, element[0])
         expect('Batman - O Longo Dia das Bruxas - Edição Definitiva').to.equal(value)
         
     })
 
     it('Excluo protudo do carrinho ', async function () {
-        page = await browser.newPage()
-
-        await page.goto('https://www.amazon.com.br/')
-        await page.waitForSelector('#twotabsearchtextbox')
-        await page.click('input[id="twotabsearchtextbox"]')
-        await page.type('input[id="twotabsearchtextbox"]', 'Batman o longo dia das Bruxas')
-        await page.click('div[class="nav-search-submit nav-sprite"]')
-        await page.waitForXPath('//span[text()="Batman - O Longo Dia das Bruxas - Edição Definitiva"]')
-        let produto = await page.$x('//span[text()="Batman - O Longo Dia das Bruxas - Edição Definitiva"]');
-        await produto[0].click()
-        await page.waitForXPath('//*[@id="productTitle"]')
-        let element = await page.$x('//*[@id="productTitle"]')
-        let value = await page.evaluate(el => el.textContent, element[0])
-        expect('\nBatman - O Longo Dia das Bruxas - Edição Definitiva\n').to.equal(value)
-        await page.click("#add-to-cart-button")
-        await page.waitForXPath('//*[@id="nav-cart-count-container"]/span[2]')
-        let carrinho = await page.$x('//*[@id="nav-cart-count-container"]/span[2]');
-        await carrinho[0].click()
-        await page.waitForXPath('//span[@class="a-truncate-full a-offscreen"]')
-        element = await page.$x('//span[@class="a-truncate-full a-offscreen"]')
-        value = await page.evaluate(el => el.textContent, element[0])
-        expect('Batman - O Longo Dia das Bruxas - Edição Definitiva').to.equal(value)
-        
-        await page.waitForXPath('//*[@value="Excluir"]')
-        element = await page.$x('//*[@value="Excluir"]')
+        await page.waitForXPath(AmaozonElementos.ExcluirProdutoCarrinho)
+        element = await page.$x(AmaozonElementos.ExcluirProdutoCarrinho)
         element[0].click()
 
         await page.waitForXPath('//*[@class="a-spacing-mini a-spacing-top-base"]')
